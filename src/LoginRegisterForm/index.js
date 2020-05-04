5ee10c1f62da578b05f963e2927af089beb92049
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-04 12:59:50 -0500

    the switch form link is working only for login and not for register

--- src/LoginRegisterForm/index.js
+++ src/LoginRegisterForm/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Button, Label } from 'semantic-ui-react'
+import '../index.css'
 
 
 export default class LoginRegisterForm extends Component {
@@ -8,20 +9,38 @@ export default class LoginRegisterForm extends Component {
 		this.state = {
 			email:'',
 			username:'',
-			passsord:''
+			passsord:'',
+			action: 'Login'
+		}
+	}
+
+	switchForm = () => {
+		if(this.state.action === "Login") {
+			this.setState({action: 'Register'})
+			console.log('switched from login to register');
+		} else {
+			this.setState({action: 'Login'})
+			console.log('switched from regeister to login');
 		}
 	}
 	render() {
 		return (
 			<React.Fragment>
+			<h2> {this.state.action} here </h2>
 				<Form>
-					<Label>Username</Label>
-					<Form.Input
-						type='text'
-						name='username'
-						placeholder="Enter Username"
-						value={this.state.username}
-					/>
+				{
+					this.state.action === "Register"
+					&&
+					<React.Fragment>
+						<Label>Username:</Label>
+						<Form.Input
+							type='text'
+							name='username'
+							placeholder="Enter Username"
+							value={this.state.username}
+						/>
+					</React.Fragment>
+				}
 					<Label>Email</Label>
 					<Form.Input
 						type='text'
@@ -36,8 +55,23 @@ export default class LoginRegisterForm extends Component {
 						placeholder="Enter Password"
 						value={this.state.password}
 					/>
-					<Button type='submit'>Log In</Button>
+					<Button type='submit'>
+					{this.state.action === "Login" ? "Log in" : "Register"}
+					</Button>
 				</Form>
+				{
+					this.state.action === 'Login'
+					?
+					<p> 
+						If you need an account you can sign up <span className='fake-link' onClick={this.switchForm}>Here</span>
+					</p>
+					:
+					<p>
+						Already have an account? log in <span className='fake-link' onClick={this.swicthForm}>Here</span>
+					</p>
+				}
+
+				
 			</React.Fragment>
 		)
 	}
