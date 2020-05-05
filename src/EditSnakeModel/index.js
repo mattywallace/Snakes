98e3678695f42bd507e842b64ad6cc5144d789d9
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-05 17:40:48 -0500

    updated the edit form

--- src/EditSnakeModel/index.js
+++ src/EditSnakeModel/index.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react'
+import { Form, Button, Label, Segment } from 'semantic-ui-react'
 
 export default class EditSnakeModel extends Component {
+	constructor(props) {
+		super(props)
+		this.state ={
+			species:'',
+			family:'',
+			habitat:'',
+			average_size:'',
+			venomous:'',
+			description:'',
+			picture:'',
+			added_by:''
+		}
+	}
+
 	render () {
-		return <h2> Edit Snake Description </h2>
+
+		return (
+			<Segment>
+				<h3>Edit the Snake Info</h3>
+				<Form>
+					<Form.Input 
+						type="text"
+						name="description"
+						value={this.state.description}
+						placeholder="Enter new description"
+					/>
+					 <Form.Input 
+			            type="text"
+			            name="picture"
+			            value={this.state.picture}
+			            placeholder="Enter a new picture"
+			          />
+			    	<Button type='Submit'>complete changes</Button>
+			    </Form>
+			</Segment>
+		)
 	}
 }
